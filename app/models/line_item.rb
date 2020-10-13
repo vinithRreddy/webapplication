8d4e4a43e7257479b7f9cd385cf12d644e878caa
Vinith Reddy <67469723+vinithRreddy@users.noreply.github.com>
2020-10-13 15:49:35 +0530

    first commit

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,9 @@
 class LineItem < ApplicationRecord
-  belongs_to :product
-  belongs_to :cart
-  def total_price
+
+  belongs_to :order, optional: true
+  belongs_to :product 
+  belongs_to :cart, optional: true
+  def total_price 
     product.price * quantity
   end
     
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,4 @@
 class Order < ApplicationRecord
-    enum pay_type: {
-        "Check" => 0,
-        "Credit card" => 1,
-        "Purchase order" => 2
-    }
-    validates :name, :address, :email, presence: true
-    validates :pay_type, inclusion: pay_types.keys
-
+    has_many :line_items, dependent: :destroy
 
 end
