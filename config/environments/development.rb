a563756f4cf475cce1dc3d876fce091638bd0772
Vinith Reddy <67469723+vinithRreddy@users.noreply.github.com>
2020-10-19 13:41:16 +0530

    commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,19 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "vinith",
+    password: "vinith11",
+    enable_starttls_auto: true
+}
+
+
+
 
   # Do not eager load code on boot.
   config.eager_load = false
